d407827e8ee758ba756472483089f6d8b33c1ee2
izzqz <v@izzqz.me>
2024-07-12 14:07:23 +0200

    Clean up deps

--- dependencies.nix
+++ dependencies.nix
@@ -1,9 +1,5 @@
 { pkgs }:
-
 with pkgs; [
-  /* Nix */
-  nil
-
   /* LLVM Debugger */
   lldb_18
 
@@ -19,12 +15,6 @@ with pkgs; [
   /* XML */
   # TODO vscode-xml-language-server
 
-  /* Dockerfile */
-  # dockerfile-language-server-nodejs
-
-  /* Terraform */
-  # terraform-ls
-
   /* Markdown */
   marksman
 
@@ -33,98 +23,7 @@ with pkgs; [
   nodePackages.typescript-language-server
   emmet-ls
 
-  /* Python */
-  # python3Packages.python-lsp-server
-
-  /* Rust */
-  # rust-analyzer
-
-  /* Go */
-  # gopls
-  # golangci-lint
-
-  /* C/C++ */
-  # clang-tools
-
-  /* Java */
-  # jdt-language-server
-
-  /* Lua */
-  # lua-language-server
-
-  /* Ruby */
-  # solargraph
-
-  /* PHP */
-  # phpactor
-
-  /* C# */
-  # omnisharp-roslyn
-
-  /* Kotlin */
-  # kotlin-language-server
-
-  /* Scala */
-  # metals
-
-  /* Haskell */
-  # haskell-language-server
-
-  /* Elixir */
-  # elixir-ls
-
-  /* OCaml */
-  # ocamlPackages.ocaml-lsp
-
-  /* Swift */
-  # sourcekit-lsp
-
   /* Zig */
   zls
   zig
-
-  /* Julia */
-  # emacsPackages.lsp-julia
-
-  /* Dart */
-  # dart
-
-  /* Elm */
-  # elmPackages.elm-language-server
-
-  /* Erlang */
-  # erlang-ls
-
-  /* Clojure */
-  # clojure-lsp
-
-  /* Crystal */
-  # crystalline
-
-  /* D */
-  # serve-d
-
-  /* F# */
-  # fsautocomplete
-
-  /* LaTeX */
-  # texlab
-
-  /* Svelte */
-  # nodePackages.svelte-language-server
-
-  /* Vue */
-  # nodePackages.vue-language-server
-
-  /* GraphQL */
-  # nodePackages.graphql-language-service-cli
-
-  /* Vala */
-  # vala-language-server
-
-  /* Ada */
-  # TODO ada_language_server
-
-  /* CMake */
-  # cmake-language-server
 ]
