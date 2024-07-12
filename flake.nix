921db9eb3a30fc4b2cec30eabefa89070fad9927
izzqz <v@izzqz.me>
2024-07-12 14:29:46 +0200

    Add flake-utils

--- flake.nix
+++ flake.nix
@@ -6,49 +6,42 @@
       url = "github:helix-editor/helix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, helix }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = system: import nixpkgs {
-        inherit system;
-        overlays = [ ];
-      };
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-          helixPackage = helix.packages.${system}.default;
-          helixDeps = import ./dependencies.nix { inherit pkgs; };
-          languagesConfig = import ./config/languages.nix { inherit pkgs; };
-          editorConfig = import ./config/editor.nix;
-          tomlFormat = pkgs.formats.toml { };
-          languagesToml = tomlFormat.generate "languages.toml" languagesConfig;
-          configToml = tomlFormat.generate "config.toml" editorConfig;
-        in
-        {
-          default = pkgs.symlinkJoin {
-            name = "helix-wrapped";
-            paths = [ helixPackage ] ++ helixDeps;
-            buildInputs = [ pkgs.makeWrapper ];
-            postBuild = ''
-              mkdir -p $out/config/helix
-              cp ${configToml} $out/config/helix/config.toml
-              cp ${languagesToml} $out/config/helix/languages.toml
-              wrapProgram $out/bin/hx \
-                --set XDG_CONFIG_HOME "$out/config" \
-                --prefix PATH : ${pkgs.lib.makeBinPath helixDeps}
-            '';
-          };
-        }
-      );
-      apps = forAllSystems (system: {
-        default = {
+  outputs = { self, nixpkgs, helix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+        helixPackage = helix.packages.${system}.default;
+        helixDeps = import ./dependencies.nix { inherit pkgs; };
+        languagesConfig = import ./config/languages.nix { inherit pkgs; };
+        editorConfig = import ./config/editor.nix;
+        tomlFormat = pkgs.formats.toml { };
+        languagesToml = tomlFormat.generate "languages.toml" languagesConfig;
+        configToml = tomlFormat.generate "config.toml" editorConfig;
+      in
+      {
+        packages.default = pkgs.symlinkJoin {
+          name = "helix-wrapped";
+          paths = [ helixPackage ] ++ helixDeps;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            mkdir -p $out/config/helix
+            cp ${configToml} $out/config/helix/config.toml
+            cp ${languagesToml} $out/config/helix/languages.toml
+            wrapProgram $out/bin/hx \
+              --set XDG_CONFIG_HOME "$out/config" \
+              --prefix PATH : ${pkgs.lib.makeBinPath helixDeps}
+          '';
+        };
+
+        apps.default = {
           type = "app";
           program = "${self.packages.${system}.default}/bin/hx";
         };
-      });
-    };
+      }
+    );
 }
